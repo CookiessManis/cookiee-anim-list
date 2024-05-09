512eadd7fe37ba70d2239d1e40d7f51f2b3dfd3a
cookiessManis <hafiz.purnama2002@gmail.com>
2024-05-09 11:57:17 +0700

    sesi-1

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,24 @@
-import AnimeListPage from "./components/AnimeList";
+import Link from "next/link";
+import AnimeListPage from "@/components/AnimeList";
 
 export default async function Home() {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime`
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?limit=8`
   );
   const anime = await response.json();
 
   return (
     <div>
-      <h1>Paling Popular</h1>
-      <div className="grid  sm:grid-cols-3 md:grid-cols-5 grid-cols-2 gap-4">
+      <div className="p-4 flex justify-between items-center">
+        <h1 className="font-bold text-2xl ">Paling Popular</h1>
+        <Link
+          href={"/all"}
+          className="md:text-xl text-md  underline font-semibold hover:text-indigo-600"
+        >
+          Lihat Semua
+        </Link>
+      </div>
+      <div className="grid  sm:grid-cols-3 md:grid-cols-4 grid-cols-2 gap-4 px-4">
         {anime.data.map((data) => {
           return (
             <div key={data.mal_id}>
--- src/components/AnimeList/index.js
+++ src/components/AnimeList/index.js
@@ -5,7 +5,13 @@ const AnimeListPage = ({ title, images, id }) => {
   return (
     // <div className="grid grid-cols-3 gap-2 h-32 ">
     <Link href={`${id}`} className="cursor-pointer">
-      <Image src={images} alt="..." width={600} height={600} />
+      <Image
+        src={images}
+        alt="..."
+        width={600}
+        height={600}
+        className="w-full h-[350px] object-fill rounded-xl "
+      />
       <h3 className="font-semibold md:text-xl text-md p-4">{title}</h3>
     </Link>
     // </div>
